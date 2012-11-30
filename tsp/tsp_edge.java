5c8b27bafc41a4ddaa6e4d34cc68462a2a3f08ef
Dejun Qian <dejun@rita.cat.pdx.edu>
2012-11-30 13:32:58 -0800

    works in somehow. a bug found: could form a T shap when adding a new edge

--- tsp/tsp_edge.java
+++ tsp/tsp_edge.java
@@ -39,7 +39,10 @@ class tsp_edge {
 	    for(Set<Edge> eset : state.visitededge) {
 		visitededge.add(new HashSet<Edge>(eset));
 	    }
-	    visitednode = new LinkedList<Set<Integer>>(state.visitednode);
+	    visitednode = new LinkedList<Set<Integer>>();
+	    for(Set<Integer> nset : state.visitednode) {
+		visitednode.add(new HashSet<Integer>(nset));
+	    }
 	    unvisited = new LinkedList<Edge>(state.unvisited);
 	    mileage = state.mileage;
 	}
@@ -49,7 +52,7 @@ class tsp_edge {
 	    this.unvisited = new LinkedList<Edge>(unvisited);
 	    this.mileage = 0;
 	}
-	public int edges() {
+	public int visitededges() {
 	    int sum = 0;
 	    for(Set<Edge> eset : visitededge) {
 		sum += eset.size();
@@ -92,44 +95,112 @@ class tsp_edge {
 
 	while(!stack.empty()) {
 	    state = stack.pop();
+	    //System.out.println(state);
 	    visitededge = state.visitededge;
 	    visitednode = state.visitednode;
 	    unvisited = state.unvisited;
-	    System.out.println("="+state.edges()+"="+instance.size()+"=");
-	    if(state.edges() < instance.size()) {
+	    //System.out.println("="+state.visitededges()+"="+instance.size()+"=");
+	    if(state.visitededges() < instance.size()-1) {
+		if(unvisited.isEmpty()) {
+		    continue;
+		}
+
 		State temp = new State(state);
 		temp.unvisited.remove(0);
 		stack.push(temp);
 
-		Set<Edge> senode1 = null;
-		Set<Edge> senode2 = null;
+		int node1 = -1;
+		int node2 = -1;
 		temp = new State(state);
-		Edge e = temp.unvisited.get(0);
-		temp.unvisited.remove(0);
+		Edge e = temp.unvisited.remove(0);
 		for(int i=0; i<temp.visitededge.size(); i++) {
 		    Set<Edge> se = temp.visitededge.get(i);
 		    Set<Integer> sn = temp.visitednode.get(i);
-		    if(sn.contains(e.node1) && sn.contains(e.node2)) {
-			senode1 = ;
-			break;
-		    } else if(sn.contains(e.node1)) {
-			se.add(e);
-			sn.add(e.node2);
-			valid = true;
-		    } else if(sn.contains(e.node2)) {
-			se.add(e);
-			sn.add(e.node1);
-			valid = true;
-		    } else {
-
+		    if(sn.contains(e.node1)) {
+			node1 = i;
+		    }
+		    if(sn.contains(e.node2)) {
+			node2 = i;
 		    }
 		}
-		stack.push(temp);
-	    } else {
-		if(best == null) {
-		} else if(best == null) {
+		if(node1==-1 && node2==-1) {
+		    Set<Edge> te = new HashSet<Edge>();
+		    te.add(e);
+		    temp.visitededge.add(te);
+		    Set<Integer> tn = new HashSet<Integer>();
+		    tn.add(e.node1);
+		    tn.add(e.node2);
+		    temp.visitednode.add(tn);
+		    temp.mileage += e.mileage;
+		    stack.push(temp);
+		} else if(node1 == -1) {
+		    Set<Edge> te = temp.visitededge.get(node2);
+		    te.add(e);
+		    Set<Integer> tn = temp.visitednode.get(node2);
+		    tn.add(e.node1);
+		    temp.mileage += e.mileage;
+		    stack.push(temp);
+		} else if(node2 == -1) {
+		    Set<Edge> te = temp.visitededge.get(node1);
+		    te.add(e);
+		    Set<Integer> tn = temp.visitednode.get(node1);
+		    tn.add(e.node2);
+		    temp.mileage += e.mileage;
+		    stack.push(temp);
+		} else if(node1 != node2) {
+		    Set<Edge> te1 = temp.visitededge.get(node1);
+		    Set<Edge> te2 = temp.visitededge.get(node2);
+		    te1.addAll(te2);
+		    temp.visitededge.remove(te2);
+		    te1.add(e);
+		    Set<Integer> tn1 = temp.visitednode.get(node1);
+		    Set<Integer> tn2 = temp.visitednode.get(node2);
+		    tn1.addAll(tn2);
+		    temp.visitednode.remove(tn2);
+		    temp.mileage += e.mileage;
+		    stack.push(temp);
 		}
 		System.out.println(state);
+	    } else if(state.visitededges() == instance.size()-1) {
+		Set<Integer> set1 = new HashSet<Integer>();
+		Set<Integer> set2 = new HashSet<Integer>();
+		for(Edge e : state.visitededge.get(0)) {
+		    if(!set1.contains(e.node1)) {
+			set1.add(e.node1);
+		    } else {
+			set2.add(e.node1);
+		    }
+		    if(!set1.contains(e.node2)) {
+			set1.add(e.node2);
+		    } else {
+			set2.add(e.node2);
+		    }
+		}
+		while(!state.unvisited.isEmpty()) {
+		    Edge e = state.unvisited.get(0);
+		    if(!(set1.contains(e.node1) && set2.contains(e.node1)) && !(set1.contains(e.node2) && set2.contains(e.node2))) {
+			state.visitededge.get(0).add(e);
+			state.mileage += e.mileage;
+			break;
+		    }
+		    state.unvisited.remove(0);
+		}
+		if(!state.unvisited.isEmpty()) {
+		    state.unvisited.clear();
+		    stack.push(state);
+		} else {
+		    //throw new RuntimeException();
+		}
+	    } else {
+		System.out.println("=>"+state);
+		if(state.mileage < 1180) {
+		    throw new RuntimeException();
+		}
+		if(best == null) {
+		    best = state;
+		} else if(state.mileage < best.mileage) {
+		    best = state;
+		}
 	    }
 	}
 
